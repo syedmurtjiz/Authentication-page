64f2f5829a10bad7d7440d153077ad81ed4b731a
Syedmurtjiz <murtjiznaqvi@gmail.com>
2024-11-18 19:59:19 +0500

    re-deploying

--- SignupPage/frontend/src/index.js
+++ SignupPage/frontend/src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom'; // Correct import without a semicolon at the end
 import 'react-toastify/ReactToastify.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root =  ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
